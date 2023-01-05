7c36879b4472a86a647753a11070554516352177
rtazaki1205 <ryo.tazaki1205@gmail.com>
2023-01-05 22:13:34 +0100

    clean up __init__.py

--- python/aggscatpy/__init__.py
+++ python/aggscatpy/__init__.py
@@ -2,22 +2,14 @@
 from aggscatpy.aggscatpy import dustmodel
 from aggscatpy.aggscatpy import distaverage
 from aggscatpy.aggscatpy import dustspec
-#from aggscatpy.aggscatpy import dustspec_header
 from aggscatpy.aggscatpy import showmodel
 from aggscatpy.aggscatpy import output_all_gallery_plot
 from aggscatpy.aggscatpy import write_data
 
 from aggscatpy.agginfo import get_sizelist
-#from aggscatpy.agginfo import check_particle_type
-#from aggscatpy.agginfo import check_particle_size
-#from aggscatpy.agginfo import check_particle_composition
-#from aggscatpy.agginfo import grs_voleq
-#from aggscatpy.agginfo import calculate_dust_mass
 from aggscatpy.agginfo import get_radius_and_porosity
-#from aggscatpy.plots import plots
 
 from aggscatpy.rendering import particle_rendering 
-#from aggscatpy.rendering import render_all
 from aggscatpy.rendering import view_particle
 
 __author__ = "Ryo Tazaki"
