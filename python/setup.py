d0b28474e05b85f9cf96f0c3c04898d94e64d34e
rtazaki1205 <ryo.tazaki1205@gmail.com>
2023-01-05 16:37:04 +0100

    added setup.py

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    description='Light scattering properties of complex particles at visible ane near infrared',
+    author='Ryo Tazaki',
+    author_email='ryo.tazaki1205@gmail.com',
+    url='https://github.com/rtazaki1205/AggScatVIR',
+    project_urls={'Documentation': 'https://rtazaki1205.github.io/AggScatVIR/'},
+    name='aggscatpy',
+    version='0.0.0',
+    license='MIT',
+    packages=['aggscatpy'],
+    install_requires=[
+        'numpy','matplotlib'
+    ],
+)
